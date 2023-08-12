d7ebe312da348664e8713e4a7f55c56ddcb404b9
JamieNewby <97129009+JamieNewby@users.noreply.github.com>
2023-08-12 20:07:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['js', 'html', 'css'],
+        salary: 1000,
+        bonus: 200,
+        getSalary: function () {
+            return this.salary + this.bonus;
+        },
+        getSkills: function () {
+            return this.skills;
+        }
+    };
+    return member;
+}
